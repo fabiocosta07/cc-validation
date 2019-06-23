cbf02fdbee8dce296345c5b1a99db35f263f28b1
efabapo <fabio.apolonio@ericsson.com>
2019-06-23 19:04:54 +0100

    changes

--- src/main/java/com/hellokoding/auth/validator/CreditCardValidator.java
+++ src/main/java/com/hellokoding/auth/validator/CreditCardValidator.java
@@ -1,5 +1,8 @@
 package com.hellokoding.auth.validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -7,6 +10,14 @@ import org.springframework.validation.Validator;
 import com.hellokoding.auth.model.CreditCard;
 @Component
 public class CreditCardValidator implements Validator {
+	
+	String regex = "^(?:(?<visa>4[0-9]{12}(?:[0-9]{3})?)|" +
+	        "(?<mastercard>5[1-5][0-9]{14})|" +
+	        "(?<discover>6(?:011|5[0-9]{2})[0-9]{12})|" +
+	        "(?<amex>3[47][0-9]{13})|" +
+	        "(?<diners>3(?:0[0-5]|[68][0-9])?[0-9]{11})|" +
+	        "(?<jcb>(?:2131|1800|35[0-9]{3})[0-9]{11}))$";
+	 
 
     @Override
     public boolean supports(Class<?> aClass) {
@@ -14,9 +25,13 @@ public class CreditCardValidator implements Validator {
     }
 
 	@Override
-	public void validate(Object target, Errors errors) {
-		// TODO Auto-generated method stub
+	public void validate(Object o, Errors errors) {
+		final CreditCard creditCard = (CreditCard)o;
+		final Pattern pattern = Pattern.compile(regex);
+	    final Matcher matcher = pattern.matcher(creditCard.getNumber());
 		
+		if (!matcher.matches())
+			errors.rejectValue("number", "CreditCard.number.invalid");
 	}
 
 }
